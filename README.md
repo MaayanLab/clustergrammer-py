4412799128b2167cd78835ef1654ca5098b37dff
cornhundred <doctorcorn2002@gmail.com>
2016-08-24 11:17:24 -0400

    adding additional required files

---
+++ README.md
@@ -0,0 +1,1 @@
+README: clustergrammer-py
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'clustergrammer',
+  packages = ['clustergrammer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python module for the Clustergrammer visualization project',
+  author = 'Nicolas Fernandez',
+  author_email = 'nickfloresfernandez@gmail.com',
+  url = 'https://github.com/MaayanLab/clustergrammer-py',
+  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/0.1',
+  keywords = ['testing'],
+  classifiers = [],
+)
