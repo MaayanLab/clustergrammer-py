06f8548455e746ebd3995ecf24e7fb227f0ddd18
cornhundred <doctorcorn2002@gmail.com>
2016-08-25 00:07:48 -0400

    incremented setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'clustergrammer',
   packages = ['clustergrammer'], # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'A python module for the Clustergrammer visualization project',
   author = 'Nicolas Fernandez',
   author_email = 'nickfloresfernandez@gmail.com',
   url = 'https://github.com/MaayanLab/clustergrammer-py',
-  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/0.1.3',
+  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/0.1.4',
   keywords = ['testing'],
   classifiers = [],
 )
