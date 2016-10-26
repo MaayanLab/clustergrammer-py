599e9134faaec9176362d120b97495f68c358fe1
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-26 12:58:47 -0400

    v1.1.2, speed up value-based categories

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'clustergrammer',
   packages = ['clustergrammer'], # this must be the same as the name above
-  version = '1.1.1',
+  version = '1.1.2',
   description = 'A python module for the Clustergrammer visualization project',
   author = 'Nicolas Fernandez',
   author_email = 'nickfloresfernandez@gmail.com',
   url = 'https://github.com/MaayanLab/clustergrammer-py',
-  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/1.1.0',
+  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/1.1.2',
   keywords = ['testing'],
   classifiers = [],
 )
