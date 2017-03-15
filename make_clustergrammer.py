15de2a34d3d6e88c56f44df674fcc433f9e1911b
cornhundred <nickfloresfernandez@gmail.com>
2017-03-14 22:28:21 -0400

    working on adding categories

--- make_clustergrammer.py
+++ make_clustergrammer.py
@@ -25,7 +25,7 @@ net.load_file('txt/rc_two_cats.txt')
 # net.random_sample(random_state=100, num_samples=10, axis='col')
 # net.clip(-6,6)
 # net.filter_cat('row', 1, 'Gene Type: Interesting')
-net.set_cat_color('col', 1, 'Category: one', 'blue')
+# net.set_cat_color('col', 1, 'Category: one', 'blue')
 
 net.make_clust(dist_type='cos',views=['N_row_sum', 'N_row_var'] , dendro=True,
              sim_mat=True, filter_sim=0.1, calc_cat_pval=False)
