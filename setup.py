9c65a67b0d9b88b17395bbe0b582b352165f2839
cornhundred <nickfloresfernandez@gmail.com>
2017-03-19 01:03:35 -0400

    version 1.8.0 improved sim_mat calc and widget export

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'clustergrammer',
   packages = ['clustergrammer'], # this must be the same as the name above
-  version = '1.7.1',
+  version = '1.8.0',
   description = 'A python module for the Clustergrammer visualization project',
   author = 'Nicolas Fernandez',
   author_email = 'nickfloresfernandez@gmail.com',
