d1c96d45a7cdebd488884d3770fba7d3e6f1a465
Daniel J. B. Clarke <u8sand@gmail.com>
2021-01-29 17:26:03 -0500

    Publish new version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
   name = 'clustergrammer',
   packages = ['clustergrammer'], # this must be the same as the name above
-  version = '1.13.6',
+  version = '1.14.0',
   description = 'A python module for the Clustergrammer visualization project',
   author = 'Nicolas Fernandez',
   author_email = 'nickfloresfernandez@gmail.com',
   url = 'https://github.com/MaayanLab/clustergrammer-py',
-  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/1.1.2',
   keywords = ['testing'],
   classifiers = [],
 )
