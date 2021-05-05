f178ce6a1e852de6327ecfe486122a6f4b5d065e
SC Van Nostrand <scvannost@gmail.com>
2021-05-05 11:43:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'clustergrammer',
-  packages = ['clustergrammer'], # this must be the same as the name above
+  name = 'clustergrammer-pandas2',
+  packages = ['clustergrammer'],
   version = '1.13.7',
   description = 'A python module for the Clustergrammer visualization project',
   author = 'Nicolas Fernandez',
   author_email = 'nickfloresfernandez@gmail.com',
-  url = 'https://github.com/MaayanLab/clustergrammer-py',
-  download_url = 'https://github.com/MaayanLab/clustergrammer-py/tarball/1.1.2',
+  url = 'https://github.com/scvannost/clustergrammer-py',
+  download_url = 'https://github.com/scvannost/clustergrammer-py/archive/refs/tags/1.13.8.tar.gz',
   keywords = ['testing'],
   classifiers = [],
 )
